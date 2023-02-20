da74db4eb6d882c7fc573f100c746ee8243e1e26
Anthoniselvi <anthoniselvi33@gmail.com>
2023-02-20 19:13:04 +0530

    updated query for upload image

--- Server.js
+++ Server.js
@@ -851,6 +851,71 @@ app.get("/events/total/all/:profileId", async (request, response) => {
 //       });
 //   });
 // });
+// const express = require('express');
+// const app = express();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+// const cors = require('cors');
+
+app.use(cors());
+app.use(express.static("./public"));
+app.use(bodyParser.json());
+
+//use of multer package
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+let maxSize = 2 * 1000 * 1000;
+let upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: maxSize,
+  },
+});
+
+let uploadHandler = upload.single("file");
+
+app.post("/upload", (req, res) => {
+  uploadHandler(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      if (err.code == "LIMIT_FILE_SIZE") {
+        res.status(400).json({ message: "Maximum file size is 2mb." });
+      }
+      return;
+    }
+
+    if (!req.file) {
+      res.status(400).json({ message: "No file!" });
+    } else {
+      res.status(200).json({ message: "Uploaded to the Server!" });
+    }
+  });
+});
+
+const imageupload = require("express-fileupload");
+
+app.use(imageupload());
+
+app.get("/home", (req, res) => {
+  res.send("image upload");
+});
+
+app.post("/images", (req, res) => {
+  if (req.files) {
+    console.log("Post image:" + req.files);
+    res.send("image uploaded");
+  }
+});
 
 app.listen(2010, () => {
   console.log("Listening successfully - use 2010");
